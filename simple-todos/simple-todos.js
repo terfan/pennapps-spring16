e049561e4256e7c9bca106e02bd8e01081001e97
ISABEL <iren@seas.upenn.edu>
2016-01-23 20:37:02 -0500

    working twitter sentiments

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,14 +1,63 @@
+//var Twit = Meteor.npmRequire('twit')
+
+/*var T = new Twit({
+    consumer_key:         'm8NoL2ItBndouXornHnfK4NpV'
+  , consumer_secret:      'iH8Nd5KDnaFsiKm6WBjtrzRfqFZIrneKzjYuwgdLD5zv6NhskH'
+  , access_token:         '18587879-1Wb4NapQZbxv7TaQkuYaolQQmm3GAX58x4x1GKI1c'
+  , access_token_secret:  'snXzqqVEsxDjjz3zvYPCk3m8au6qLbrrpI3dIQgeamVXi'
+})*/
+
+function getTweets(s)
+{
+    Meteor.call("getTweetHTTP", [s], function(error, results) {
+        //console.log("TEST" + results); //results.data should be a JSON object
+        //var score = results.data["text"];
+        //$("#tweet").text(s + " " + );
+        //return score
+        console.log(error, results)
+        return results
+        //return Tweets.count();
+    });
+}
+
 function getSentiment(s)
 {
     Meteor.call("getAlchHTTP", [s], function(error, results) {
         //console.log(results.data["docSentiment"]["score"]); //results.data should be a JSON object
         var score = results.data["docSentiment"]["score"];
         $("#location").text(s + " " + score);
-        return score;
+        return score
     });
 }
 
 if (Meteor.isServer) {
+
+    Meteor.methods({
+        getTweetHTTP: function (text) {
+            this.unblock();
+            var Twit = Meteor.npmRequire('twit');
+            var T = new Twit({
+              consumer_key:         'm8NoL2ItBndouXornHnfK4NpV'
+            , consumer_secret:      'iH8Nd5KDnaFsiKm6WBjtrzRfqFZIrneKzjYuwgdLD5zv6NhskH'
+            , access_token:         '18587879-1Wb4NapQZbxv7TaQkuYaolQQmm3GAX58x4x1GKI1c'
+            , access_token_secret:  'snXzqqVEsxDjjz3zvYPCk3m8au6qLbrrpI3dIQgeamVXi'
+          })
+
+            T.get('search/tweets', { q: text, count: 5}, Meteor.bindEnvironment(function(err, data, response) {
+              var results = data["statuses"];
+              for (var i = 0; i < results.length; i++) {
+                console.log("tweet: "+ results[i].text);
+                url = "http://gateway-a.watsonplatform.net/calls/text/TextGetTextSentiment?apikey=0e2d5d3c35387b178bd6f8d27c2e44ca83fbc5aa&outputMode=json&text=" + results[i].text;
+
+                var score = Meteor.http.call("GET", url).data["docSentiment"]["score"]
+                  //Tweets.insert({text: results[i].text});
+                  Tweets.insert({text: results[i].text + " " + score});
+              }
+              //return data["statuses"]
+            }))
+        }
+    });
+
     Meteor.methods({
         getAlchHTTP: function (text) {
             this.unblock();
@@ -19,8 +68,16 @@ if (Meteor.isServer) {
     });
 }
  
+Tweets = new Mongo.Collection("tweets");
+
 if (Meteor.isClient) {
   // This code only runs on the client
+
+  Template.body.helpers({
+     tweets: function () {
+        return Tweets.find({});
+      }
+    });
 
 Template.body.events({
     "submit .new-location": function (event) {
@@ -30,11 +87,35 @@ Template.body.events({
       // Get value from form element
       var text = event.target.text.value;
 
-    getSentiment(text);
+      getSentiment(text);
 
  
       // Clear form
       event.target.text.value = "";
+    },
+
+    "submit .tweet": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var text = event.target.text.value;
+
+      //getSentiment(text);
+      //getTweets(text);
+      Meteor.call("getTweetHTTP", [text], function(error, results) {
+        //console.log("TEST" + results); //results.data should be a JSON object
+        //var score = results.data["text"];
+        //$("#tweet").text(s + " " + );
+        //return score
+        //console.log(results)
+        return results
+        //return Tweets.count();
+    });
+ 
+      // Clear form
+      event.target.text.value = "";
     }
+
   });
 }
